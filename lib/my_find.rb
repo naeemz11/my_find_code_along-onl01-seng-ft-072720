32bc6fbbb9584e3fd76d15d0a73add3dc3d1aad0
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 22:29:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,9 @@ def my_find(collection)
 empty_arr=[ ]
   counter= 0
 while counter < collection.length
-return empty_arr << collection[counter] if yield(collection[counter])
-else 
-  return nil
+return collection[counter] << if yield(collection[counter])
 counter += 1
-end 
+ 
 end 
   empty_arr
 end 
