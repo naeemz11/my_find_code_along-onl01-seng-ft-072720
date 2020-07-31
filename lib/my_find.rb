6940b0debff20b4c742d31fb735e0934159d9ab1
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 22:37:02 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
+empty_arr=[]
+  counter= 0
+while counter < collection.length
+return empty_arr << collection[counter] if yield(collection[counter])
+counter += 1
+ 
+end 
+  empty_arr
+end 
 
-end
