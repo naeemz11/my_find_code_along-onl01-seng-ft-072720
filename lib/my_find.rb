2b34430607f8f4ecea6f90d30d460ecd5eb475c0
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 21:51:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-  empty_arr= []
-  
+
   counter= 0
 while counter < collection.lentgth 
-r
+return collection[counter] if yield collection[counter]
+counter += 1 
 
 end 
 end
